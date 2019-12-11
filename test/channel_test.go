fa516eb03f914434de5c1dd3b8fa2fe085878423
Anatolie Lupacescu <anatollupacescu@gmail.com>
2019-12-11 21:41:17 +0100

    http and channels example

---
+++ test/channel_test.go
@@ -0,0 +1,37 @@
+package test
+
+import (
+	"fmt"
+	"log"
+	"testing"
+	"time"
+)
+
+func TestProducer(t *testing.T) {
+	messages := make(chan int)
+	quit := make(chan struct{})
+
+	go func() {
+		var i int
+		for {
+			select {
+			case messages <- i:
+				i++
+				fmt.Println("sent message")
+			case <-quit:
+				fmt.Println("done")
+				close(messages)
+				return
+			}
+		}
+	}()
+
+	for i := 0; i < 5; i++ {
+		msg := <-messages
+		log.Println(msg)
+	}
+
+	quit <- struct{}{}
+	close(quit)
+	time.Sleep(time.Second)
+}

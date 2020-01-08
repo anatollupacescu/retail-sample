ea5dbde9bcab35c9aa667a31089b104ab58cc3f4
Anatolie Lupacescu <anatollupacescu@gmail.com>
2020-01-08 10:15:46 +0100

    parcel react initial

---
+++ parcel-react/src/entrypoint.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import { render } from 'react-dom'
+// App wrapped with redux Provider, store, etc.
+
+import { App } from '~/ui/App'
+
+render(
+  <App />,
+  document.getElementById('root')
+)
---
+++ parcel-react/src/ui/App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+
+export class App extends React.Component {
+  public render() {
+    return (
+      <div>
+        <title>Dashboard</title>
+        <h1>Jora tractorist</h1>
+      </div>
+    )
+  }
+}

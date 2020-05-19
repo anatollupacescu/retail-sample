88cd4e565bb8dbf010708570aec80fba3ab33fd7
Anatolie Lupacescu <anatollupacescu@gmail.com>
2020-05-19 16:19:15 +0200

    refactor inventory tests

--- internal/retail-domain/inventory/inventory_test.go
+++ internal/retail-domain/inventory/inventory_test.go
@@ -1,6 +1,7 @@
 package inventory_test
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -8,44 +9,17 @@ import (
 	"github.com/anatollupacescu/retail-sample/internal/retail-domain/inventory"
 )
 
-func TestInventory(t *testing.T) {
-
-	t.Run("should have no registered names on creation", func(t *testing.T) {
-		mockStore := &inventory.MockStore{}
-
-		var emptyResp []inventory.Item
-		mockStore.On("List").Return(emptyResp, nil)
-
-		i := inventory.Inventory{Store: mockStore}
-		names, err := i.List()
-
-		assert.NoError(t, err)
-		assert.Len(t, names, 0)
-	})
-
-	t.Run("should return zero ID for missing name", func(t *testing.T) {
-		t.Skip("will return an error")
-		mockStore := &inventory.MockStore{}
-		i := inventory.Inventory{Store: mockStore}
-
-		mockStore.On("Find", "test").Return(0, nil)
-
-		id, err := i.Find("test")
-
-		assert.NoError(t, err)
-		assert.Equal(t, id, 0)
-		mockStore.AssertExpectations(t)
-	})
-
-	t.Run("should reject empty name", func(t *testing.T) {
-		var mockStore inventory.Store
-		i := inventory.Inventory{Store: mockStore}
+func TestAdd(t *testing.T) {
+	t.Run("when name is empty", func(t *testing.T) {
+		i := inventory.Inventory{}
 		_, err := i.Add("")
 
-		assert.Equal(t, inventory.ErrEmptyName, err)
+		t.Run("throws empty name error", func(t *testing.T) {
+			assert.Equal(t, inventory.ErrEmptyName, err)
+		})
 	})
 
-	t.Run("should reject duplicate name", func(t *testing.T) {
+	t.Run("when name is already present", func(t *testing.T) {
 		milk := "milk"
 
 		mockStore := &inventory.MockStore{}
@@ -55,77 +29,65 @@ func TestInventory(t *testing.T) {
 
 		id, err := i.Add(milk)
 
-		assert.Equal(t, inventory.ErrDuplicateName, err)
-		assert.Zero(t, id)
-		mockStore.AssertExpectations(t)
+		t.Run("calls the store", func(t *testing.T) {
+			mockStore.AssertExpectations(t)
+		})
+
+		t.Run("throws duplicate name error", func(t *testing.T) {
+			assert.Equal(t, inventory.ErrDuplicateName, err)
+			assert.Zero(t, id)
+		})
 	})
 
-	t.Run("should persist when given a valid name", func(t *testing.T) {
-		t.SkipNow()
-
+	t.Run("when name is valid", func(t *testing.T) {
 		milk := "milk"
 
 		mockStore := &inventory.MockStore{}
 		i := inventory.Inventory{Store: mockStore}
 
-		mockStore.On("Find", milk).Return(0, nil)
+		mockStore.On("Find", milk).Return(0, inventory.ErrItemNotFound)
 		mockStore.On("Add", milk).Return(1, nil)
 
 		id, err := i.Add(milk)
 
-		assert.NoError(t, err)
-		assert.Equal(t, id, 1)
-		mockStore.AssertExpectations(t)
+		t.Run("calls the store", func(t *testing.T) {
+			mockStore.AssertExpectations(t)
+		})
+
+		t.Run("saves it to store", func(t *testing.T) {
+			assert.NoError(t, err)
+			assert.Equal(t, 1, id)
+		})
 	})
 
-	t.Run("should log successfully added name", func(t *testing.T) {
-		t.SkipNow()
-
+	t.Run("when store throws error during find op", func(t *testing.T) {
 		milk := "milk"
 
 		mockStore := &inventory.MockStore{}
 		i := inventory.Inventory{Store: mockStore}
 
-		mockStore.On("Find", milk).Return(0, nil)
-		mockStore.On("Add", milk).Return(1, nil)
+		expected := errors.New("unknown")
+		mockStore.On("Find", milk).Return(0, expected)
 
 		id, err := i.Add(milk)
 
-		assert.NoError(t, err)
-		assert.Equal(t, id, 1)
-		mockStore.AssertExpectations(t)
+		t.Run("calls the store", func(t *testing.T) {
+			mockStore.AssertExpectations(t)
+		})
+
+		t.Run("should propage it to the caller", func(t *testing.T) {
+			assert.Equal(t, expected, err)
+			assert.Equal(t, 0, id)
+		})
 	})
+}
 
-	t.Run("should provide full list of names", func(t *testing.T) {
-		mockStore := &inventory.MockStore{}
-		i := inventory.Inventory{Store: mockStore}
-
-		mockStore.On("List").Return([]inventory.Item{
-			{
-				ID:   1,
-				Name: "test",
-			},
-		}, nil)
-
-		records, err := i.List()
-
-		assert.NoError(t, err)
-		assert.Len(t, records, 1)
-
-		mockStore.AssertExpectations(t)
-	})
-
-	t.Run("should return ID for correct name", func(t *testing.T) {
-		mockStore := &inventory.MockStore{}
-		i := inventory.Inventory{Store: mockStore}
-
-		mockStore.On("Find", "test").Return(1, nil)
-
-		id, err := i.Find("test")
-
-		assert.NoError(t, err)
-		assert.Equal(t, id, 1)
-
-		mockStore.AssertExpectations(t)
-	})
+func TestList(t *testing.T) {
+	t.Run("calls store", func(t *testing.T) {})
+}
+func TestFind(t *testing.T) {
+	t.Run("calls store", func(t *testing.T) {})
+}
+func TestGet(t *testing.T) {
+	t.Run("calls store", func(t *testing.T) {})
 }

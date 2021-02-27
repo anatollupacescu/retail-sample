dcd0923936e49a5a395082fc427dd45e9ea2c2b9
Anatolie Lupacescu <anatollupacescu@gmail.com>
2021-02-27 10:40:50 +0200

    add doc to the skele file

--- domain/retail/inventory/doc.go
+++ domain/retail/inventory/doc.go
@@ -1,2 +1,4 @@
-// Package inventory manages a list of inventory items, allows to add and disable individual items
+// Inventory is a collection of items that can be part of the stock and recipe
+// Inventory items can be added or disabled. Disabled items can not added to a new recipe or provisioned in stock
+
 package inventory
